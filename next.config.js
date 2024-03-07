38410a0f021d06a849946629124b93fc91bc4e24
Devansh Purohit <25509659+devd-99@users.noreply.github.com>
2024-03-06 21:25:23 -0500

    working tree clean

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**",
-      },
-    ],
-  },
+  images: { unoptimized: true },
   output: 'export'
 };
 
